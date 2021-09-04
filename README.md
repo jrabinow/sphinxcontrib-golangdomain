ae08b3487c4d029fdf1f271960694314c36b1369
Julien Rabinow <randprefix.github@fastmail.com>
2021-09-03 17:01:46 -0700

    A touch of spring-cleaning for README.md, setup.py and gitignore

--- README.md
+++ README.md
@@ -5,12 +5,13 @@
 
 ## Installing
 
+You will need to make sure [godocjson](https://github.com/readthedocs/godocjson.git) is installed and added to your `$PATH` environment variable.  
 You can install the package from source using the setup.py file.
 
 ```
 git clone https://github.com/chrisdoherty4/sphinxcontrib-golangdomain
 cd sphinxcontrib-golangdomain
-pip install .
+python setup.py install
 ```
 
 From PyPi:
@@ -18,6 +19,11 @@ From PyPi:
 ```
 pip install <TODO>
 ```
+
+## Configuration
+
+Add `autoapi/index` to one of your `.rst` doc files and checkout the [sphinx-autoapi tutorial](https://sphinx-autoapi.readthedocs.io/en/latest/tutorials.html#go)
+You should be good to go!
 
 ## License
 
--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ long_desc = """
 This package contains the sphinxcontrib-golangdomain Sphinx extension.
 
 This extension adds golang Domain to Sphinx.
-It needs Sphinx 1.0 or newer.
+It needs Sphinx 1.0 or newer, and sphinx-autoapi 1.8.3 or newer
 """
 
-requires = ["Sphinx>=1.0"]
+requires = ["Sphinx>=1.0" "sphinx-autoapi>=1.8.3"]
 
 setup(
     name="sphinxcontrib-golangdomain",
     version="0.2.0",
-    url="https://github.com/jrabinow/sphinxcontrib-golangdomain",
-    download_url="https://github.com/jrabinow/sphinxcontrib-golangdomain",
+    url="https://github.com/chrisdoherty4/sphinxcontrib-golangdomain",
+    download_url="https://github.com/chrisdoherty4/sphinxcontrib-golangdomain",
     license="BSD",
-    author="Yoshifumi YAMAGUCHI",
-    author_email="ymotongpoo at gmail.com",
+    author="Chris Doherty",
+    author_email="chris dot doherty4 at gmail dot com",
     description="Sphinx extension sphinxcontrib-golangdomain",
     long_description=long_desc,
     zip_safe=False,
